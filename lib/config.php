90a56d5bf11d55416ee8c07a20dbf319f76e96ae
Aaron Parecki <aaron@parecki.com>
2013-02-16 12:52:48 -0800

    basic index.php router

--- lib/config.php
+++ lib/config.php
@@ -1,8 +1,8 @@
 <?php
 class Config {
-  public static $dbHost = 'db.node';
-  #public static $dbHost = '127.0.0.1';
-  public static $dbName = '500lattes';
-  public static $dbUsername = '500lattes';
+  #public static $dbHost = 'db.node';
+  public static $dbHost = '127.0.0.1';
+  public static $dbName = 'indienews';
+  public static $dbUsername = 'indienews';
   public static $dbPassword = 'coffee';
 }
---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+chdir('..');
+require 'vendor/autoload.php';
+require 'lib/Savant.php';
+require 'lib/helpers.php';
+require 'lib/config.php';
+
+// Configure the Savant plugin
+\Slim\Extras\Views\Savant::$savantDirectory = 'lib/Savant3';
+\Slim\Extras\Views\Savant::$savantOptions = array('template_path' => 'views');
+
+// Set up the database connection
+ORM::configure('mysql:host=' . Config::$dbHost . ';dbname=' . Config::$dbName);
+ORM::configure('username', Config::$dbUsername);
+ORM::configure('password', Config::$dbPassword);
+
+// Create a new app object with the Savant view renderer
+$app = new \Slim\Slim(array(
+  'view' => new \Slim\Extras\Views\Savant()
+));
+
+require 'controllers/controllers.php';
+
+$app->run();
+
