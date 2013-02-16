69f7a45f61c855b452c5cf320df5ebaaad52929d
Aaron Parecki <aaron@parecki.com>
2013-02-16 12:46:12 -0800

    first commit with Savant3

---
+++ lib/config.php
@@ -0,0 +1,8 @@
+<?php
+class Config {
+  public static $dbHost = 'db.node';
+  #public static $dbHost = '127.0.0.1';
+  public static $dbName = '500lattes';
+  public static $dbUsername = '500lattes';
+  public static $dbPassword = 'coffee';
+}
