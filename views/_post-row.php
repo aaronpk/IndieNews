12f6b76c7e91d55e29e45e28d557380ffae9cea1
Aaron Parecki <aaron@parecki.com>
2013-02-19 05:20:59 -0800

    Show URL for posts if no title exists

--- views/_post-row.php
+++ views/_post-row.php
@@ -5,7 +5,7 @@
     <a href="/vote?post=<?= $this->post->id ?>&amp;vote=down" class="down"><i class="icon-arrow-down"></i></a>
   </td>
   <td>
-    <div class="title"><a href="<?= $this->post->href ?>"><?= $this->post->title ?></a></div>
+    <div class="title"><a href="<?= $this->post->href ?>"><?= $this->post->title ?: preg_replace('/^https?:\/\//', '', $this->post->href) ?></a></div>
     <div class="details"><?= $this->post->domain ?> | <?= $this->post->points ?> point<?= $this->post->points == 1 ? '' : 's' ?> | <?= $this->post->date_submitted ?> | <a href="/post/<?= $this->post->id ?>"><?= $this->post->comments ?> comment<?= $this->post->comments == 1 ? '' : 's' ?></a></div>
   </td>
 </tr>
