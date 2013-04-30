6179a2acdaff8f80b339b3d217924a3e5499d2aa
Aaron Parecki <aaron@parecki.com>
2013-04-30 12:49:23 -0700

    update comment instructions on a post. adds <action> element for commenting

--- views/post.php
+++ views/post.php
@@ -10,14 +10,17 @@
     <td></td>
     <td></td>
     <td>
-      <h5>How to comment</h5>
-      <p>To comment on this post, you should:
-        <ol>
-          <li>Create a <a href="http://indiewebcamp.com/post">post</a> on your own domain</li>
-          <li>link to this page, or to one of the individual comments, preferably with the <a href="http://indiewebcamp.com/in-reply-to">in-reply-to</a> markup</li>
-          <li>send a <a href="http://indiewebcamp.com/webmention">webmention</a> notification</li>
-        </ol>
-      </p>
+      <h5>Comment</h5>
+      <action inline do="reply" with="<?= $this->post->href ?>">
+        <p>To comment on this post, you should:
+          <ol>
+            <li>Create a <a href="http://indiewebcamp.com/post">post</a> on your own domain</li>
+            <li>link to the original post with the <a href="http://indiewebcamp.com/in-reply-to">in-reply-to</a> markup</li>
+            <li>add a <a href="http://indiewebcamp.com/rel-syndication">u-syndication</a> link to IndieNews</li>
+            <li>send a <a href="http://indiewebcamp.com/webmention">webmention</a> notification</li>
+          </ol>
+        </p>
+      </action>
     </td>
   </tr>
   <?php foreach($this->replies as $i=>$post): ?>
