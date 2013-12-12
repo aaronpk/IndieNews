0a228f6f4ba28bf62a5e9b01288f0f8d4df2d7ea
Aaron Parecki <aaronpk@pkpro.local>
2013-12-12 09:53:29 -0800

    move to template config

--- lib/config.template.php
+++ lib/config.template.php
@@ -2,11 +2,10 @@
 class Config {
   public static $hostname = 'news.indiewebcamp.com';
   
-  #public static $dbHost = 'db.node';
   public static $dbHost = '127.0.0.1';
   public static $dbName = 'indienews';
   public static $dbUsername = 'indienews';
-  public static $dbPassword = 'coffee';
+  public static $dbPassword = '';
   public static $gaid = '';
   public static $fbappid = '';
 }
