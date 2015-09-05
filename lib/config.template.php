aed95d0401ba77f858baa595d90868c39c8b527c
Aaron Parecki <aaron@parecki.com>
2015-09-05 16:05:04 -0700

    drop fb/google crap

--- lib/config.template.php
+++ lib/config.template.php
@@ -1,11 +1,10 @@
 <?php
 class Config {
   public static $hostname = 'news.indiewebcamp.com';
+  public static $baseURL = 'http://news.indiewebcamp.com';
   
   public static $dbHost = '127.0.0.1';
   public static $dbName = 'indienews';
   public static $dbUsername = 'indienews';
   public static $dbPassword = '';
-  public static $gaid = '';
-  public static $fbappid = '';
 }
--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="IndieNews">
-    <link rel="pingback" href="http://pingback.me/webmention?forward=http://<?=$_SERVER['SERVER_NAME']?>/webmention" />
+    <link rel="pingback" href="https://webmention.io/webmention?forward=http://<?=$_SERVER['SERVER_NAME']?>/webmention" />
     <link rel="webmention" href="https://<?=$_SERVER['SERVER_NAME']?>/webmention" />
 
     <?= $this->meta ?>
@@ -26,41 +26,6 @@
   </head>
 
 <body>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<?= Config::$fbappid ?>";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', '<?= Config::$gaid ?>']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<script>
-  window.fbAsyncInit = function() {
-    FB.Event.subscribe('edge.create', function(targetUrl) {
-      _gaq.push(['_trackSocial', 'facebook', 'like', targetUrl]);
-    });
-    FB.Event.subscribe('edge.remove', function(targetUrl) {
-      _gaq.push(['_trackSocial', 'facebook', 'unlike', targetUrl]);
-    });
-    FB.Event.subscribe('message.send', function(targetUrl) {
-      _gaq.push(['_trackSocial', 'facebook', 'send', targetUrl]);
-    });
-  };
-</script>
-
 
 <div class="navbar navbar-static-top">
   <div class="navbar-inner">
