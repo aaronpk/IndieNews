5e071975f772b8f973b655dee2cd27d1dd1c5d67
Aaron Parecki <aaron@parecki.com>
2015-09-05 19:04:38 -0700

    update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,14 +9,14 @@ CREATE TABLE `posts` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) unsigned DEFAULT NULL,
   `date_submitted` datetime NOT NULL,
-  `source_url` varchar(512) DEFAULT NULL,
   `post_date` datetime DEFAULT NULL,
   `post_author` varchar(255) DEFAULT NULL,
   `title` varchar(512) DEFAULT NULL,
   `href` varchar(512) DEFAULT NULL,
   `body` text,
-  `in_reply_to` varchar(512) DEFAULT NULL,
   `comments` int(11) NOT NULL DEFAULT '0',
+  `in_reply_to` varchar(512) DEFAULT NULL,
+  `source_url` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
