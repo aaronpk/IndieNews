b08a1a9282e0010be08ef3fed65f7cd8a447c1d4
Aaron Parecki <aaron@parecki.com>
2015-09-08 11:18:54 -0700

    add instructions for submitting a new language

--- views/index.php
+++ views/index.php
@@ -14,3 +14,9 @@
     <a href="/de">Deutsch</a>
   </div>
 </div>
+
+<div class="row" style="margin-top: 20px;">
+  <div class="span12">
+    <p>Want to see a new language? You can add a translation as a pull request! Copy the <a href="https://github.com/aaronpk/IndieNews/blob/master/Locale/de/default.po">Locale</a> file to a new folder, fill it out for your language, and submit a pull request!</p>
+  </div>
+</div>
